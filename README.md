caa2824da146a7fcddb7d3c7e7467f5396dbc7d5
Kazuki Tobita <kazukiti201@gmail.com>
2025-09-28 10:35:41 +0900

    Add archives

--- README.md
+++ README.md
@@ -16,3 +16,7 @@ crw-rw-rw- 9,10 root wheel 27 Sep 11:04 /dev/tty.usbmodem1101
 - エディタ下部の ✅️ でビルド
 - エディタ下部の ➡️ で書き込み
 - なお、書き込み時にはM5Stack本体のリセットボタンを2秒間長押しし、ダウンロードモードに移行しておく(LEDが光る)
+
+## archivesディレクトリについて
+
+- `archives` 配下にある `main.cpp` を実行したい場合、そのコードを `src/main.cpp` に配置する
---
+++ archives/hello_world_gui/main.cpp
@@ -0,0 +1,22 @@
+#include <M5CoreS3.h>
+
+auto& Display = M5.Display;
+
+void setup() {
+  auto cfg = M5.config();
+  M5.begin(cfg);
+
+  uint16_t bg = Display.color565(0x22, 0x22, 0x22);
+  uint16_t fg = Display.color565(0xFF, 0xFF, 0xFF);
+
+  Display.setRotation(1);
+  Display.fillScreen(bg);
+  Display.setTextColor(fg, bg);
+  Display.setTextSize(3);
+  Display.setCursor(20, 20);
+  Display.print("hello, world!");
+}
+
+void loop() {
+  M5.update();
+}
