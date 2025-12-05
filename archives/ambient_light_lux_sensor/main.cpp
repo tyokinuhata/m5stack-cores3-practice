dd8499cab160e393f4a7efa3d1a9601c21b9b265
Kazuki Tobita <kazukiti201@gmail.com>
2025-12-05 12:02:04 +0900

    Modify dlight

--- archives/ambient_light_lux_sensor/main.cpp
+++ archives/ambient_light_lux_sensor/main.cpp
@@ -5,7 +5,7 @@
 auto& Display = CoreS3.Display;
 
 // Ambient Light Unit(BH1750FVI)のセンサオブジェクト
-M5_DLight DLight;
+M5_DLight dLight;
 
 void setup() {
   auto cfg = M5.config();
@@ -16,16 +16,16 @@ void setup() {
   Display.setTextSize(3);
 
   // 初期化
-  DLight.begin(&Wire, CoreS3.Ex_I2C.getSDA(), CoreS3.Ex_I2C.getSCL());
+  dLight.begin(&Wire, CoreS3.Ex_I2C.getSDA(), CoreS3.Ex_I2C.getSCL());
   // 測定モードの設定(標準的なモード。連続/分解能は1lx/測定速度は120ms)
-  DLight.setMode(CONTINUOUSLY_H_RESOLUTION_MODE);
+  dLight.setMode(CONTINUOUSLY_H_RESOLUTION_MODE);
 }
 
 void loop() {
   CoreS3.update();
 
   // lx（ルクス）値の取得
-  uint16_t lux = DLight.getLUX();
+  uint16_t lux = dLight.getLUX();
 
   Display.setCursor(20, 20);
   Display.printf("Lux: %-5u", lux);
